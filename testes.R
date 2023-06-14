0ccab247632b4cf6bf334474e90bb439952ec1c6
itatrajano <34098524+itatrajano@users.noreply.github.com>
2023-06-14 21:46:23 +0000

    Testando instalação de pacotes

--- testes.R
+++ testes.R
@@ -1,2 +1,10 @@
-mensagem <- "Hello World"
-print(mensagem)
+#Instalando um novo pacote
+
+#Instalando o pacote ggplot2
+install.packages("ggplot2")
+
+#Carregando o pacote ggplot2
+library(ggplot2)
+
+# ggplot 2 é um pacote de visualização de dados - utilizado para a criação de gráficos
+?ggplot2
