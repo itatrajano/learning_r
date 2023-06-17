75600516cfdfc3efce25d828f357709d522a52a6
itatrajano <34098524+itatrajano@users.noreply.github.com>
2023-06-16 20:06:39 -0400

    Mais exercicios

--- testes.R
+++ testes.R
@@ -1,10 +1,33 @@
 #Instalando um novo pacote
 
-#Instalando o pacote ggplot2
+##Instalando o pacote ggplot2
 install.packages("ggplot2")
 
 #Carregando o pacote ggplot2
-library(ggplot2)
+#library(ggplot2)
 
 # ggplot 2 é um pacote de visualização de dados - utilizado para a criação de gráficos
-?ggplot2
+#?ggplot2
+
+#Vetores
+#cidades <- c("Brasilia", "Rio de Janeiro", "Sao Paulo", "Curitiba", "Macapa")
+#print(cidades)
+#temperaturas <- c(20, 37, 13, 4, 41)
+#print(temperaturas)
+#regioes <- c(3, 4, 4, 5, 1)
+#print(regioes)
+#cidade <- cidades[3]
+#print(cidade)
+#excluir a temperatura 4 graus, quarta posicao
+#temperaturas <- c(temperaturas[-4])
+#temperatura <- temperaturas[2:4]
+#print(temperatura)
+#temperaturas[4] <- 4
+#temperaturas[5] <- 41
+#temperaturas <- NULL
+#estados <- factor(c("DF", "RJ", "SP", "PR", "AP"))
+#print(estados)
+grau.instrucao <- factor(c("Medio", "Fundamental", "Superior"), 
+                         levels = c("Fundamental", "Medio", "Superior"),
+                         ordered = TRUE)
+grau.instrucao
